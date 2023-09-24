13881f1b723a65201959f517e6a67fc6e90f8422
Kulukov Yuriy <tuberkulum@gmail.com>
2023-09-24 20:50:14 +0400

    созает сервер на express

--- nodeJs_course/server/server.js
+++ nodeJs_course/server/server.js
@@ -6,6 +6,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
     console.log('Server request');
+
     res.setHeader('Content-Type', 'text/html');
 
     const createPath = (page) =>
