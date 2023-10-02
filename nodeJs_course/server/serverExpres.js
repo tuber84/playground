82973526311222655f8071533cbee099fc164f26
Kulukov Yuriy <tuberkulum@gmail.com>
2023-10-02 21:30:57 +0400

    убирает статику

--- nodeJs_course/server/serverExpres.js
+++ nodeJs_course/server/serverExpres.js
@@ -10,48 +10,59 @@ const createPath = (page) => path.resolve(__dirname, 'ejs-html', `${page}.ejs`);
 
 app.listen(PORT, 'localhost', (error) => {
     error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
 
-    // app.use((req, pes, next) => {
-    //     console.log(`path: ${req.path}`);
-    //     console.log(`path: ${req.method}`);
-    //     next();
-    // });
-    app.use(
-        morgan(':method :url :status :res[content-lenght] - response-time ms')
-    );
+app.use(morgan(':method :url :status :res[content-lenght] - response-time ms'));
 
-    app.use(express.static('styles'));
+app.use(express.static('styles')); // подключаем стили,добавляем папку стилей в исключение
 
-    app.get('/', (req, res) => {
-        const title = 'Home';
-        res.render(createPath('index'), { title });
-    });
-    app.get('/contacts', (req, res) => {
-        const title = 'Contacts';
-        const contacts = [
-            { name: 'Youtube', link: 'https://3dnews.ru/' },
-            { name: 'Twitter', link: 'https://3dnews.ru/' },
-        ];
-        res.render(createPath('contacts'), { contacts, title });
-    });
-    app.get('/posts:id', (req, res) => {
-        const title = 'Post';
-        res.render(createPath('posts'), { title });
-    });
-    app.get('/posts', (req, res) => {
-        const title = 'Posts';
-        res.render(createPath('posts'), { title });
-    });
-    app.get('/add-post', (req, res) => {
-        const title = 'Add post';
-        res.render(createPath('add-post'), { title });
-    });
-    app.get('/contacts2', (req, res) => {
-        res.redirect('/contacts');
-    });
+app.get('/', (req, res) => {
+    const title = 'Home';
+    res.render(createPath('index'), { title });
+});
+app.get('/contacts', (req, res) => {
+    const title = 'Contacts';
+    const contacts = [
+        { name: 'Youtube', link: 'https://3dnews.ru/' },
+        { name: 'Twitter', link: 'https://3dnews.ru/' },
+    ];
+    res.render(createPath('contacts'), { contacts, title });
+});
+app.get('/posts/:id', (req, res) => {
+    const title = 'new post';
+    const post = {
+        id: '1',
+        text: 'sdafgsdgsdksdaadkj   fgsdfgdfg  dfgsgsdf  34j  dsfsdfgd  wfsg lghlasl',
+        title: 'Заготоловок поста',
+        date: '02.10.2023',
+        author: 'YuRock',
+    };
+    res.render(createPath('post'), { title, post });
+});
+app.get('/posts', (req, res) => {
+    const title = 'какой то текст';
+    const posts = [
+        {
+            id: '1',
+            text: 'sdafgsdgsdksdaadkj   fgsdfgdfg  dfgsgsdf  34j  dsfsdfgd  wfsg lghlasl',
+            title: 'Заготоловок поста',
+            date: '02.10.2023',
+            author: 'YuRock',
+        },
+    ];
 
-    app.use((req, res) => {
-        const title = 'Error';
-        res.status(404).render(createPath('error'), { title });
-    });
+    res.render(createPath('posts'), { title, posts });
+});
+
+app.get('/add-post', (req, res) => {
+    const title = 'Add post';
+    res.render(createPath('add-post'), { title });
+});
+app.get('/contacts2', (req, res) => {
+    res.redirect('/contacts');
+});
+
+app.use((req, res) => {
+    const title = 'Error';
+    res.status(404).render(createPath('error'), { title });
 });
