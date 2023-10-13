d82470292b54c469200aa5028aae8ca225dd0c63
Kulukov Yuriy <tuberkulum@gmail.com>
2023-10-13 23:30:21 +0400

    простой таймер js

---
+++ timer/app.js
@@ -0,0 +1,50 @@
+class App {
+  timer = {
+    min_tens: document.getElementById("min_tens"),
+    min: document.getElementById("min"),
+    sec_tens: document.getElementById("sec_tens"),
+    sec: document.getElementById("sec"),
+  };
+  #interval;
+
+  submit(event) {
+    event.preventDefault;
+    const formData = new FormData(event.target); // получаем данные из формы
+    const time = formData.get("time");
+    this.#clearTimer();
+    this.#startTimer(time);
+  }
+
+  #clearTimer() {
+    if (this.#interval) {
+      clearInterval(this.#interval);
+    }
+    this.#setTimer({
+      min_tens: 0,
+      min: 0,
+      sec_tens: 0,
+      sec: 0,
+    });
+  }
+
+  #startTimer(time) {
+    const emd = Date.now + time * 1000 * 60;
+    this.#interval = setInterval(() => {
+      const now = Date.now();
+      const delta = end - now;
+      if (delta < 0) {
+        clearInterval(this.#interval);
+        return;
+      }
+    }, 500);
+  }
+
+  #setTimer({ min_tens, min, sec, sec_tens }) {
+    this.timer.sec_tens.innerText = sec_tens;
+    this.timer.sec.innerText = sec;
+    this.timer.min.innerText = min;
+    this.timer.min_tens.innerText = min_tens;
+  }
+}
+
+const app = new App();
