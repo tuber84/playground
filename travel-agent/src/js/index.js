e28118dea273887b9efe95b19a22c14e2ea04889
Kulukov Yuriy <tuberkulum@gmail.com>
2024-03-08 14:32:01 +0400

    swiper слайдер

--- travel-agent/src/js/index.js
+++ travel-agent/src/js/index.js
@@ -1,6 +1,18 @@
 // import mobileNav from './modules/mobile-nav.js';
 // mobileNav();
 
-import autoCompleteFunc from "./modules/autoComplete";
+// import autoCompleteFunc from "./modules/autoComplete";
 
-autoCompleteFunc();
+// autoCompleteFunc();
+
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 4,
+  spaceBetween: 32,
+  navigation: {
+    nextEl: "#sliderPrev",
+    prevEl: "#sliderNext",
+  },
+});
